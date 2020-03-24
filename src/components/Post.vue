9c0c3ef57df7c25f3cf5126e8df12a13fe17fb5d
Aptemf <fil.a42.ru@live.ru>
2020-03-24 20:17:48 +0700

    Update Post.vue

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -3,13 +3,14 @@
             class="mx-auto post-form"
             max-width="500"
     >
-        <v-btn color="red" icon @click="deleteBtn" max-width="25px" max-height="25px">
-            <v-icon>mdi-delete</v-icon>
-        </v-btn>
+
 
         <v-expansion-panels style="margin-bottom: 5px">
         <v-expansion-panel>
-            <v-expansion-panel-header>{{post.title}}
+            <v-btn color="red" icon @click="deleteBtn" max-width="25px" max-height="25px">
+                <v-icon>mdi-delete</v-icon>
+            </v-btn>
+            <v-expansion-panel-header>          {{post.title}}
 
             </v-expansion-panel-header>
             <v-expansion-panel-content>{{ post.body }}</v-expansion-panel-content>
